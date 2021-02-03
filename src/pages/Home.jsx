a695800cf7740a52b0d0ef8012ac388b454d8474
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-02-03 16:20:15 +0100

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { FormControl, Container, Button } from 'react-bootstrap'
+class Home extends Component {
+    state = {
+        location: "",
+        position: "",
+        result: {},
+    }
+
+    render() {
+        let position = this.state.position
+        let location = this.state.location
+        return (
+            <Container className="m-auto">
+                <FormControl
+                    value={this.state.position} onChange={
+                        e => this.setState({ position: e.currentTarget.value })
+                    } />
+                <FormControl
+                    value={this.state.location} onChange={
+                        e => this.setState({ location: e.currentTarget.value })
+                    } />
+                <Button onClick={
+                    async function fetchFunc() {
+                        fetch("cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=" + position + "&location=" + location)
+                            .then(async (res) => {
+                                console.log(res)
+                            })
+                        // .then(result => {
+                        //     this.setState({ result: this.data })
+                        //     console.log(result)
+                        // })
+                    }
+                }>Submit</Button>
+            </Container >
+        )
+    }
+}
+
+export default Home
